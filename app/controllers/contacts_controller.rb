4de60f2845dfec911cf7e2a53605c08f8ae3f36b
Jesse Jacobs <cobbs87@gmail.com>
2017-04-01 18:36:56 +0000

    Added Hirb gem for data presentation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,6 +14,6 @@ class ContactsController < ApplicationController
   
   private
     def contact_params
-      params.require(:contact).permit(:name, :email, :coments)
+      params.require(:contact).permit(:name, :email, :comments)
     end
 end
