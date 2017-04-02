2c95cccdf15594b819ce0a3e0d6efa90b8895c18
Jesse Jacobs <cobbs87@gmail.com>
2017-04-02 13:23:09 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'jessemjacobs1@gmail.com'  
+  default to: 'cobbs87@gmail.com'  
   
   def contact_email(name, email, body)
     @name = name
