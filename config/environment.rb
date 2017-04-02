4775efa0d014541f3ee4545435ef28d48d9b8602
Jesse Jacobs <cobbs87@gmail.com>
2017-04-02 21:28:13 +0000

    Fixed syntax error in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :port           => Env['MAILGUN_SMTP_PORT'],
+  :port           => ENV['MAILGUN_SMTP_PORT'],
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
